6f9a75baff38fabdf31f271cb818b1868d3ce95c
unknown <adrianjocos@hotmail.com>
2024-01-21 07:27:15 -0500

    change save

--- src/main/java/com/adriancostaiv/libs/data/Main.java
+++ src/main/java/com/adriancostaiv/libs/data/Main.java
@@ -1,13 +1,15 @@
-package com.adriancostaiv.libs.data.reader;
+package com.adriancostaiv.libs.data;
 
+import com.adriancostaiv.libs.data.constant.url.AllUrl;
 import com.adriancostaiv.libs.data.logger.ILogger;
+import com.adriancostaiv.libs.data.reader.BalReader;
 import com.adriancostaiv.libs.data.writer.Writer;
 
 public class Main {
     static final ILogger logger = ILogger.getLogger();
     public static void main(String[] args)  {
         try {
-            Writer.writeString("target/baloto.csv", BalReader.getAllDatas());
+            Writer.writeString("target/" + AllUrl.BALNAME, BalReader.getAllDatas());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/adriancostaiv/libs/data/constant/url/AllUrl.java
+++ src/main/java/com/adriancostaiv/libs/data/constant/url/AllUrl.java
@@ -2,6 +2,7 @@ package com.adriancostaiv.libs.data.constant.url;
 
 public class AllUrl {
     public static final String BAL = "https://baloto.com/resultados-baloto/";
+    public static final String BALNAME = "baloto.csv";
     public static final int BALI = 2081;
     public static final String BALINIT = "container-balls-results";
     public static final String BALEND = "row mt-3";
